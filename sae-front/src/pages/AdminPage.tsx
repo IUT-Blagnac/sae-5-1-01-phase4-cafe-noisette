c9bc9cbc28830dd87a35145467460d58b2aacfa9
Nolan <nolan.jacquemont@gmail.com>
2023-12-13 20:01:29 +0100

    updated admin page

--- sae-front/src/pages/AdminPage.tsx
+++ sae-front/src/pages/AdminPage.tsx
@@ -1,11 +1,209 @@
-import {useAuthUser} from "../contexts/AuthUserContext";
-import {ProjectPreferencesSelect} from "../components/ProjectPreferencesSelect";
-import {useState} from "react";
-import {Project} from "../models/Project";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
+import {
+    GridRowsProp,
+    GridRowModesModel,
+    GridRowModes,
+    DataGrid,
+    GridColDef,
+    GridActionsCellItem,
+    GridEventListener,
+    GridRowId,
+    GridRowModel,
+    GridRowEditStopReasons, GridToolbar,
+} from '@mui/x-data-grid';
+import {useEffect, useState} from "react";
+import {User} from "../models/User";
+import {getUsersFromIds} from "../models/Project";
+import {getUsers} from "../rest/queries";
 
-export const AdminPage = () => {
-    const user = useAuthUser()
-    const [projects, setProjects] = useState<Project[]>([])
 
-    return <ProjectPreferencesSelect projects={projects} setProjects={setProjects} />
+const roles = ['Market', 'Finance', 'Development'];
+const randomRole = () => {
+    return ['Market']
+};
+
+const initialRows: GridRowsProp = [
+    {
+        id: 1,
+        name: "blabla",
+        age: 25,
+        joinDate: new Date(),
+        role: randomRole(),
+    },
+    {
+        id: 2,
+        name: "blabla22",
+        age: 25,
+        joinDate: new Date(),
+        role: randomRole(),
+    },
+];
+
+export default function AdminPage() {
+    const [rows, setRows] = React.useState(initialRows);
+    const [users, setUsers] = useState<User[] | undefined>()
+    const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>({});
+
+    useEffect(() => {
+        if (!users) {
+            getUsers().then((u) => {
+                setUsers(u.data)
+            })
+        }
+    }, [users]);
+
+    const handleRowEditStop: GridEventListener<'rowEditStop'> = (params, event) => {
+        if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+            event.defaultMuiPrevented = true;
+        }
+    };
+
+    const handleEditClick = (id: GridRowId) => () => {
+        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+    };
+
+    const handleSaveClick = (id: GridRowId) => () => {
+        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+    };
+
+    const handleDeleteClick = (id: GridRowId) => () => {
+        setRows(rows.filter((row) => row.id !== id));
+    };
+
+    const handleCancelClick = (id: GridRowId) => () => {
+        setRowModesModel({
+            ...rowModesModel,
+            [id]: { mode: GridRowModes.View, ignoreModifications: true },
+        });
+
+        const editedRow = rows.find((row) => row.id === id);
+        if (editedRow!.isNew) {
+            setRows(rows.filter((row) => row.id !== id));
+        }
+    };
+
+    const processRowUpdate = (newRow: GridRowModel) => {
+        const updatedRow = { ...newRow, isNew: false };
+        setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+        return updatedRow;
+    };
+
+    const handleRowModesModelChange = (newRowModesModel: GridRowModesModel) => {
+        setRowModesModel(newRowModesModel);
+    };
+
+    const columns: GridColDef[] = [
+        { field: 'name', headerName: 'Name', width: 180, editable: true },
+        {
+            field: 'age',
+            headerName: 'Age',
+            type: 'number',
+            width: 80,
+            align: 'left',
+            headerAlign: 'left',
+            editable: true,
+        },
+        {
+            field: 'joinDate',
+            headerName: 'Join date',
+            type: 'date',
+            width: 180,
+            editable: true,
+        },
+        {
+            field: 'role',
+            headerName: 'Department',
+            width: 220,
+            editable: true,
+            type: 'singleSelect',
+            valueOptions: ['Market', 'Finance', 'Development'],
+        },
+        {
+            field: 'actions',
+            type: 'actions',
+            headerName: 'Actions',
+            width: 100,
+            cellClassName: 'actions',
+            getActions: ({ id }) => {
+                const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+                if (isInEditMode) {
+                    return [
+                        <GridActionsCellItem
+                            icon={<SaveIcon />}
+                            label="Save"
+                            sx={{
+                                color: 'primary.main',
+                            }}
+                            onClick={handleSaveClick(id)}
+                        />,
+                        <GridActionsCellItem
+                            icon={<CancelIcon />}
+                            label="Cancel"
+                            className="textPrimary"
+                            onClick={handleCancelClick(id)}
+                            color="inherit"
+                        />,
+                    ];
+                }
+
+                return [
+                    <GridActionsCellItem
+                        icon={<EditIcon />}
+                        label="Edit"
+                        className="textPrimary"
+                        onClick={handleEditClick(id)}
+                        color="inherit"
+                    />,
+                    <GridActionsCellItem
+                        icon={<DeleteIcon />}
+                        label="Delete"
+                        onClick={handleDeleteClick(id)}
+                        color="inherit"
+                    />,
+                ];
+            },
+        },
+    ];
+
+    return (
+        <Box
+            sx={{
+                height: 500,
+                width: '100%',
+                '& .actions': {
+                    color: 'text.secondary',
+                },
+                '& .textPrimary': {
+                    color: 'text.primary',
+                },
+            }}
+        >
+            <DataGrid
+                sx={{
+                    mr: 5,
+                    ml: 5,
+                    mt: 15
+                }}
+                rows={rows}
+                columns={columns}
+                editMode="row"
+                rowModesModel={rowModesModel}
+                onRowModesModelChange={handleRowModesModelChange}
+                onRowEditStop={handleRowEditStop}
+                processRowUpdate={processRowUpdate}
+                slots={{ toolbar: GridToolbar }}
+                slotProps={{
+                    toolbar: { setRows, setRowModesModel },
+                }}
+            />
+        </Box>
+    );
 }
