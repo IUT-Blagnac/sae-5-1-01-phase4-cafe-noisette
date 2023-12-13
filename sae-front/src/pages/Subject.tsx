a7eed36703eed0076c67bdfd391e51a0eebf9c08
EternalNico <nicolas.sacilotto22@gmail.com>
2023-12-13 12:18:17 +0100

    lack of back but front is ok

--- sae-front/src/pages/Subject.tsx
+++ sae-front/src/pages/Subject.tsx
@@ -5,164 +5,202 @@ import { useAuthUser } from '../contexts/AuthUserContext';
 import { getAllTeams, getProjects } from '../rest/queries';
 import { Team } from '../models/Team';
 import { Project } from '../models/Project';
+import { useTheme } from '../utils/theme';
 
-interface Task {
-  id: number;
-  title: string;
-  status: string;
+interface SubjectProps {}
+
+export interface ExtendedTeam extends Omit<Team, 'projectId'> {
+  id?: number;
+  projectId?: number | undefined;
 }
 
-interface TaskItemProps {
-  task: Task;
-  onDrop: (taskId: number, newStatus: string) => void;
-}
-
-interface TaskListProps {
-  title: string;
-  tasks: Task[];
-  status: string;
-  onDrop: (taskId: number, newStatus: string) => void;
-}
-
-const EmptyDropZone: React.FC<{ onDrop: (newStatus: string) => void }> = ({ onDrop }) => {
-  const [, drop] = useDrop({
-    accept: 'TASK',
-    drop: () => onDrop(''),
-    canDrop: () => true,
-  });
-
-  return (
-    <div
-      ref={(node) => drop(node)}
-      style={{ border: '1px dashed #ccc', minHeight: '50px', margin: '8px' }}
-    >
-      (Déposer ici)
-    </div>
-  );
-};
-
-const TaskItem: React.FC<TaskItemProps> = ({ task, onDrop }) => {
-  const [, dragRef] = useDrag({
-    type: 'TASK',
-    item: { id: task.id, status: task.status },
-  });
-
-  return (
-    <div
-      ref={dragRef}
-      style={{
-        border: '1px solid #ccc',
-        padding: '8px',
-        margin: '8px',
-        cursor: 'move',
-      }}
-    >
-      {task.title}
-    </div>
-  );
-};
-
-const TaskList: React.FC<TaskListProps> = ({ title, tasks, status, onDrop }) => {
-    const [projects, setProjects] = React.useState([] as Project[])
-    useEffect(() => {
-        // Appel à getProjects pour récupérer les sujets 
-        getProjects().then((response) => {
-          if (response.responseCode === 200 && response.data) {
-            //map du name
-            setProjects(response.data);
-
-          } else {
-            console.log(response.data);
-          }
-        });
-      }, [projects]); 
-  
-    const [, drop] = useDrop({
-    accept: 'TASK',
-    drop: (item: { id: number; status: string }) => onDrop(item.id, status),
-    canDrop: () => true,
-  });
-
-  return (
-    <div
-      ref={(node) => drop(node)}
-      style={{ border: '1px solid #ddd', padding: '16px', borderRadius: '4px', width: '200px', minHeight: '100px' }}
-    >
-      <h3>{title}</h3>
-      <EmptyDropZone onDrop={(newStatus) => onDrop(-1, newStatus)} />
-      {tasks
-        .filter((task) => task.status === status)
-        .map((task) => (
-          <TaskItem key={task.id} task={task} onDrop={onDrop} />
-        ))}
-    </div>
-  );
-};
-
-const Subject: React.FC = () => {
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [teams, setTeams] = React.useState([] as Team[])
-  const authUser = useAuthUser();
+const Subject: React.FC<SubjectProps> = () => {
+  const [teams, setTeams] = useState<ExtendedTeam[]>([]);
+  const [tasks, setTasks] = useState<{ id: number; title: string; status: string; teamIds?: number[] }[]>([]);
+  const [showResetButton, setShowResetButton] = useState(false);
+  const [showSaveButton, setShowSaveButton] = useState(false);
+  const theme = useTheme();
 
   useEffect(() => {
-    // Appel à getAllTeams pour récupérer les équipes
     getAllTeams().then((response) => {
       if (response.responseCode === 200 && response.data) {
-        //map du name
         setTeams(response.data);
-      } else {
-        console.log(response.data);
-      }
-    });
-  }, [teams]); // Utilisation de useEffect pour appeler getAllTeams 
-
- 
-  useEffect(() => {
-    // Appel à getProjects pour récupérer les sujets
-    getProjects().then((response) => {
-      if (response.responseCode === 200 && response.data) {
-        // Utilisation directe du résultat dans setTasks
-        setTasks(response.data.map((project) => ({
-          id: project.id || 0, // Assure que id n'est jamais undefined
-          title: project.name,
-          status: 'undefined',
-        })));
       } else {
         console.log(response.data);
       }
     });
   }, []);
 
-  const handleDrop = (taskId: number, newStatus: string) => {
-    if (newStatus && taskId === -1) {
-      // If dropping in an empty zone, add the new task at the end
-      setTasks([...tasks, { id: tasks.length + 1, title: `Task ${tasks.length + 1}`, status: newStatus }]);
-    } else if (!newStatus && taskId !== -1) {
-      // If dragging from the list to a section, remove the task from the list
-      setTasks(tasks.filter((task) => task.id !== taskId));
-    } else {
-      // If dragging within sections, update the task status
-      setTasks((prevTasks) =>
-        prevTasks.map((task) => (task.id === taskId ? { ...task, status: newStatus } : task))
-      );
+  useEffect(() => {
+    getProjects().then((response) => {
+      if (response.responseCode === 200 && response.data) {
+        setTasks(
+          response.data.map((project) => ({
+            id: project.id || 0,
+            title: project.name,
+            status: 'undefined',
+          }))
+        );
+      } else {
+        console.log(response.data);
+      }
+    });
+  }, []);
+
+  const handleDrop = (teamId: number, projectId: number) => {
+    const team = teams.find((team) => team.id === teamId);
+
+    if (team && team.projectId !== null && team.projectId !== undefined) {
+      console.log(`Team ${teamId} is already assigned to a project.`);
+      return;
     }
+
+    const updatedTeams = teams.map((team) =>
+      team.id === teamId ? { ...team, projectId } : team
+    );
+    setTeams(updatedTeams);
+
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === projectId) {
+        const teamIds = task.teamIds || [];
+        return { ...task, teamIds: [...teamIds, teamId] };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+
+    setShowResetButton(true);
+    setShowSaveButton(true);
+    
+    console.log(`Team ${teamId} dropped on Project ${projectId}`);
+    console.log(`Team ${teamId} is now in Project ${projectId}`);
+  };
+
+  const handleReset = () => {
+    const resetTeams = teams.map((team) => ({ ...team, projectId: undefined as number | undefined }));
+    setTeams(resetTeams);
+  
+    const resetTasks = tasks.map((task) => ({ ...task, teamIds: [] }));
+    setTasks(resetTasks);
+  
+    setShowResetButton(false);
+    setShowSaveButton(true);
+  };
+  
+  const handleSave = () => {
+    // Logique pour sauvegarder les modifications
+  
+    setShowResetButton(false);
+    setShowSaveButton(false);
+  };
+
+  const TeamItem: React.FC<{ team: ExtendedTeam }> = ({ team }) => {
+    const [, drag] = useDrag({
+      type: 'TEAM',
+      item: { id: team.id || 0 },
+    });
+    
+    const teamItemStyle: React.CSSProperties = {
+      backgroundColor: theme.palette.secondary.main,
+      border: '1px solid black',
+      marginBottom: '5px',
+      padding: '8px',
+    };
+    
+    return <div ref={drag} style={teamItemStyle}>{team.name}</div>;
+  };
+  
+  const ProjectItem: React.FC<{ task: { id: number; title: string; status: string; teamIds?: number[] } }> = ({ task }) => {
+    const [, drop] = useDrop({
+      accept: 'TEAM',
+      drop: (item: { type: string; id: number }) => handleDrop(item.id, task.id),
+    });
+
+    const projectItemStyle: React.CSSProperties = {
+      backgroundColor: theme.palette.secondary.main,
+      border: '1px solid black',
+      marginBottom: '5px',
+      padding: '8px',
+      color: 'inherit'
+    };
+
+    return (
+      <div ref={drop} style={projectItemStyle}>
+        <div>
+          <strong>{task.title}</strong>
+        </div>
+        {task.teamIds && task.teamIds.length > 0 && (
+          <div>
+            <strong>Teams:</strong>
+            <ul>
+              {task.teamIds.map((teamId) => (
+                <li key={teamId}>{teams.find((team) => team.id === teamId)?.name}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  const subjectContainerStyle: React.CSSProperties = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    marginBottom: '20px',
+  };
+
+  const teamListStyle: React.CSSProperties = {
+    width: '200px',
+    padding: '10px',
+    marginRight: '10%'
+  };
+
+  const projectListStyle: React.CSSProperties = {
+    width: '200px',
+    padding: '10px',
+    marginLeft: '10%'
+  };
+
+  const buttonStyle: React.CSSProperties = {
+    backgroundColor: theme.palette.secondary.main,
+    color: 'inherit',
+    border: '1px solid black',
+    padding: '8px',
+    cursor: 'pointer',
+    marginTop: '10px',
+    textAlign: 'center',
+    width: '50%',
+    margin: 'auto',
   };
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <div style={{ display: 'flex', justifyContent: 'space-around', paddingTop: '20px' }}>
-        <div style={{ width: '200px', marginRight: '20px' }}>
-          <h2>Liste des sujets</h2>
-          {tasks
-            .filter((task) => task.status === 'undefined')
-            .map((task) => (
-              <TaskItem key={task.id} task={task} onDrop={handleDrop} />
-            ))}
+      <div style={subjectContainerStyle}>
+        <div style={teamListStyle}>
+          <h2>Teams</h2>
+          {teams.map((team) => (
+            <TeamItem key={team.id} team={team} />
+          ))}
         </div>
-        {teams.map((team, index) => (
-          <TaskList key={index} title={team.name} tasks={tasks} status={`Section ${index + 1}`} onDrop={handleDrop} />
-        ))}
+        <div style={projectListStyle}>
+          <h2>Projects</h2>
+          {tasks.map((task) => (
+            <ProjectItem key={task.id} task={task} />
+          ))}
+        </div>
       </div>
+      {showResetButton && (
+        <div onClick={handleReset} style={buttonStyle}>
+          Reset Teams
+        </div>
+      )}
+      {showSaveButton && (
+        <div onClick={handleSave} style={buttonStyle}>
+          Save Changes
+        </div>
+      )}
     </DndProvider>
   );
 };
