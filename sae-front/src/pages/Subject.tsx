70bc7ab813dd8533d3278cf87a66eb4f305f5b72
EternalNico <nicolas.sacilotto22@gmail.com>
2023-12-14 20:17:35 +0100

    finition of Subject page

--- sae-front/src/pages/Subject.tsx
+++ sae-front/src/pages/Subject.tsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useDrag, useDrop, DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { useAuthUser } from '../contexts/AuthUserContext';
-import { addMemberTeam, addProjectTeam, getAllTeams, getProjects } from '../rest/queries';
+import { addMemberTeam, addProjectTeam, deleteProjectTeam, getAllTeams, getProjects } from '../rest/queries';
 import { Team } from '../models/Team';
 import { Project } from '../models/Project';
 import { useTheme } from '../utils/theme';
 import toast from 'react-hot-toast';
+import { inherits } from 'util';
 
 interface SubjectProps {}
 
@@ -18,8 +19,8 @@ export interface ExtendedTeam extends Omit<Team, 'projectId'> {
 const Subject: React.FC<SubjectProps> = () => {
   const [teams, setTeams] = useState<ExtendedTeam[]>([]);
   const [tasks, setTasks] = useState<{ id: number; title: string; status: string; teamIds?: number[] }[]>([]);
-  const [showResetButton, setShowResetButton] = useState(false);
-  const [showSaveButton, setShowSaveButton] = useState(false);
+  const [showResetButton, setShowResetButton] = useState(true);
+  const [showSaveButton, setShowSaveButton] = useState(true);
   const theme = useTheme();
 
   useEffect(() => {
@@ -50,17 +51,17 @@ const Subject: React.FC<SubjectProps> = () => {
 
   const handleDrop = (teamId: number, projectId: number) => {
     const team = teams.find((team) => team.id === teamId);
-
+  
     if (team && team.projectId !== null && team.projectId !== undefined) {
-      console.log(`Team ${teamId} is already assigned to a project.`);
+      toast.error('Cette équipe est déjà affectée à un projet');
       return;
     }
-
+  
     const updatedTeams = teams.map((team) =>
       team.id === teamId ? { ...team, projectId } : team
     );
     setTeams(updatedTeams);
-
+  
     const updatedTasks = tasks.map((task) => {
       if (task.id === projectId) {
         const teamIds = task.teamIds || [];
@@ -69,49 +70,78 @@ const Subject: React.FC<SubjectProps> = () => {
       return task;
     });
     setTasks(updatedTasks);
+  
 
-    setShowResetButton(true);
-    setShowSaveButton(true);
-    
+  
     console.log(`Team ${teamId} dropped on Project ${projectId}`);
     console.log(`Team ${teamId} is now in Project ${projectId}`);
   };
 
   const handleReset = () => {
+    teams.forEach((team) => {
+      if (team.projectId !== undefined) {
+        const teamForDelete = team as Team; 
+        console.log(teamForDelete)
+        deleteProjectTeam(teamForDelete, team.id ?? 0).then((response) => {
+          if (response.responseCode === 200) {
+            if (response.data) {
+              toast.success('Les modifications ont été réinitialisées');
+            }
+          } else {
+            console.log('Error while removing project from team: ' + response.errorMessage);
+          }
+        });
+      }
+    });
+
     const resetTeams = teams.map((team) => ({ ...team, projectId: undefined as number | undefined }));
     setTeams(resetTeams);
-  
+
     const resetTasks = tasks.map((task) => ({ ...task, teamIds: [] }));
     setTasks(resetTasks);
-  
-    setShowResetButton(false);
-    setShowSaveButton(true);
+
   };
+
   
-  const handleSave = () => {
-    console.log(teams)
-    console.log(tasks)
-    // Logique pour sauvegarder les modifications
-    {tasks.filter((task) => task.teamIds).map((task) => 
-      {task.teamIds?.map((teamId) => {
-        const team = teams.find((team) => team.id === teamId) as Team
-        console.log(team)
-        addProjectTeam(team, teamId).then((response) => {
-          if (response.responseCode === 200) {
-              if (response.data) {
-                  toast.success("Les modifications ont été sauvegardées")
-              }
-          } else {
-              console.log("Error while add project in team: " + response.errorMessage);
-          }
-      }
-  )
-      }
-      )}
-      )}
+
+  const handleSave = async () => {
+    try {
+      console.log('Saving changes...');
+      
+      // Save changes to the server
+      await Promise.all(
+        tasks
+          .filter((task) => task.teamIds)
+          .flatMap((task) =>
+            task.teamIds?.map((teamId) => {
+              const team = teams.find((team) => team.id === teamId) as Team;
+              console.log('Saving project for team:', team);
+              return addProjectTeam(team, teamId);
+            }) || []
+          )
+      );
   
-    setShowResetButton(false);
-    setShowSaveButton(false);
+      console.log('Changes saved successfully');
+  
+      // Update local state to reflect the changes
+      const updatedTeams = await getAllTeams();
+      setTeams(updatedTeams.data || []);
+  
+      const resetTasks = tasks.map((task) => ({
+        ...task,
+        teamIds: [],
+      }));
+      setTasks(resetTasks);
+  
+      console.log('Reset tasks:', resetTasks);
+  
+
+  
+      toast.success('Les modifications ont été sauvegardées');
+    } catch (error) {
+      console.error('Error while saving changes:', error);
+      toast.error('Une erreur est survenue lors de la sauvegarde des modifications');
+    }
   };
 
   const TeamItem: React.FC<{ team: ExtendedTeam }> = ({ team }) => {
@@ -119,17 +149,17 @@ const Subject: React.FC<SubjectProps> = () => {
       type: 'TEAM',
       item: { id: team.id || 0 },
     });
-    
+
     const teamItemStyle: React.CSSProperties = {
       backgroundColor: theme.palette.secondary.main,
       border: '1px solid black',
       marginBottom: '5px',
       padding: '8px',
     };
-    
+
     return <div ref={drag} style={teamItemStyle}>{team.name}</div>;
   };
-  
+
   const ProjectItem: React.FC<{ task: { id: number; title: string; status: string; teamIds?: number[] } }> = ({ task }) => {
     const [, drop] = useDrop({
       accept: 'TEAM',
@@ -141,7 +171,7 @@ const Subject: React.FC<SubjectProps> = () => {
       border: '1px solid black',
       marginBottom: '5px',
       padding: '8px',
-      color: 'inherit'
+      color: 'inherit',
     };
 
     return (
@@ -172,32 +202,77 @@ const Subject: React.FC<SubjectProps> = () => {
   };
 
   const teamListStyle: React.CSSProperties = {
-    width: '200px',
+    width: '20%',
     padding: '10px',
-    marginRight: '10%'
+    marginLeft: '10%',
+    textAlign: 'center',
+    border: '3px solid black',
+    marginTop: '1.4%'
   };
-
+  
   const projectListStyle: React.CSSProperties = {
-    width: '200px',
+    width: '20%',
     padding: '10px',
-    marginLeft: '10%'
+    marginLeft: '10%',
+    textAlign: 'center',
+    border: '3px solid purple',
+    marginTop: '1.4%'
   };
 
+  const buttonContainerStyle: React.CSSProperties = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '100%',
+    padding: '20px', // Ajout de marge intérieure pour éloigner les boutons du bord
+  };
+  
   const buttonStyle: React.CSSProperties = {
-    backgroundColor: theme.palette.secondary.main,
     color: 'inherit',
-    border: '1px solid black',
+    border: `1px solid ${theme.palette.primary.main}`, // Bordure avec la couleur du thème
     padding: '8px',
     cursor: 'pointer',
     marginTop: '10px',
     textAlign: 'center',
-    width: '50%',
-    margin: 'auto',
+    width: '48%',
   };
 
+  const teamsWithProject = teams.filter((team) => team.projectId !== null && team.projectId !== undefined);
+  const teamsWithoutProject = teams.filter((team) => team.projectId === null || team.projectId === undefined);
+  
+  const tableHeaderStyle: React.CSSProperties = {
+    border: '1px solid #dddddd',
+    padding: '8px',
+    textAlign: 'center',
+
+  };
+  
+  const tableCellStyle: React.CSSProperties = {
+    border: '1px solid #dddddd',
+    padding: '8px',
+  };
   return (
     <DndProvider backend={HTML5Backend}>
+      <h1><center>Glisser les équipes dans le projet de votre choix</center></h1>
       <div style={subjectContainerStyle}>
+        <div style={{ width: '400px', padding: '10px', border: '3px solid purple', marginTop: '1.4%' }}>
+          <h2><center>les équipes</center></h2>
+          <table style={{ width: '100%', borderCollapse: 'collapse', marginTop: '10px' }}>
+            <thead>
+              <tr>
+                <th style={tableHeaderStyle}>Équipes avec Projet</th>
+                <th style={tableHeaderStyle}>Équipes sans Projet</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Array.from({ length: Math.max(teamsWithProject.length, teamsWithoutProject.length) }).map((_, index) => (
+                <tr key={index}>
+                  <td style={tableCellStyle}>{teamsWithProject[index]?.name}</td>
+                  <td style={tableCellStyle}>{teamsWithoutProject[index]?.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
         <div style={teamListStyle}>
           <h2>Teams</h2>
           {teams.map((team) => (
@@ -211,16 +286,18 @@ const Subject: React.FC<SubjectProps> = () => {
           ))}
         </div>
       </div>
-      {showResetButton && (
-        <div onClick={handleReset} style={buttonStyle}>
-          Reset Teams
-        </div>
-      )}
-      {showSaveButton && (
-        <div onClick={handleSave} style={buttonStyle}>
-          Save Changes
-        </div>
-      )}
+      <div style={buttonContainerStyle}>
+        {showResetButton && (
+          <div onClick={handleReset} style={buttonStyle}>
+            Reset Teams
+          </div>
+        )}
+        {showSaveButton && (
+          <div onClick={handleSave} style={buttonStyle}>
+            Save Changes
+          </div>
+        )}
+      </div>
     </DndProvider>
   );
 };
